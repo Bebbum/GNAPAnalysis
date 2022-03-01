b3e45569b3092334d75874349f445da29bc3fd9a
Andrew Webb <adw0022@uah.edu>
2022-03-01 11:24:40 -0600

    added more comments, changed the structure of the message, and had the AS sign the token with the inverse of its generated asymmetric key. The output finishes before hitting the strand limit set to 100

--- GNAP_Simple/GNAP_simple_scenario.scm
+++ GNAP_Simple/GNAP_simple_scenario.scm
@@ -1,19 +1,21 @@
 (herald "Grant Negotiation and Authorization Protocol"
-	(limit 50)
+	(limit 100)
 	(comment "This protocol allows a piece of software, the client instance, to request delegated authorization to resource servers and to request direct information"))
 
 (defprotocol single_token_simple basic
 	(defrole client
-		(vars (c as rs name) (access acess_token value rights response data) (n1 n2 text) (k akey))
+		(vars (c as rs name) (access acess_token value rights response data) (n1 n2 n3 text) (k akey))
 		(trace
-			; This assumes communcation over encrypted TLS connection (ltk c as)
+			; (Section 12.16 p.123) Since TLS protects the entire HTTP message in transit, verification of the TLS client certificate
+			; presented with the message provides a sufficient binding between the two.			
+			; In this case we assume communcation over encrypted TLS connection with (ltk c as) as the certificate
 			; (Section 12.5 p.114) With asymmetric keys, the client needs only to send its public key to  the AS to allow for 
 			; verification that the client holds the associated private key, regardless of whether that key was pre-registered
 			; or not with the AS.
 			(send (enc c access (pubk c) (ltk c as))) ; client requests access at the AS by identifying itself and proving possession of a cryptogrphic key
-			(recv (enc n1 (enc (cat acess_token value rights) k) (cat (pubk c) (ltk c as)))) ; receives and decrypts message using private key whic results in a usable access token
-			(send (enc n2 (enc (cat acess_token value rights) k) (cat (pubk c) (ltk c rs)))) ; The client instance uses the access token to call the RS.
-			(recv (enc response (cat (pubk c) (ltk c rs))))
+			(recv (enc (enc n1 (enc (cat acess_token value rights) (invk k)) (pubk c)) (ltk c as))) ; receives and decrypts message using private key whic results in a usable access token
+			(send (enc (enc n2 (enc (cat acess_token value rights) (invk k)) (pubk c)) (ltk c rs))) ; The client instance uses the access token to call the RS.
+			(recv (enc (enc response (pubk c)) (ltk c rs)))
 		)
 	)
 	(defrole authorization_server
@@ -29,14 +31,14 @@
 			; (Section 12.8 p.117) Key proofing mechanisms used with access tokens need to use replay protection mechanisms covered
 			; under the signature such as a per-message nonce, a reasonably short time validity window, or other uniqueness 
 			; constraints.
-			(send (enc n1 (enc (cat acess_token value rights) k) (cat (pubk c) (ltk c as)))) ; AS signs token and binds it to client's public key
+			(send (enc (enc n1 (enc (cat acess_token value rights) (invk k)) (pubk c)) (ltk c as))) ; AS signs token and binds it to client's public key
 		)
 	)
 	(defrole resource_server
-		(vars (c rs name) (acess_token value rights response data) (n2 text) (k akey))
+		(vars (c rs name) (acess_token value rights response data) (n2 n3 text) (k akey))
 		(trace
-			(recv (enc n2 (enc (cat acess_token value rights) k) (cat (pubk c) (ltk c rs)))) ; RS decrypts and validates the client's token
-			(send (enc response (cat (pubk c) (ltk c rs))))
+			(recv (enc (enc n2 (enc (cat acess_token value rights) (invk k)) (pubk c)) (ltk c rs))) ; RS decrypts and validates the client's token
+			(send (enc (enc response (pubk c)) (ltk c rs)))
 		)
 	)
 )
